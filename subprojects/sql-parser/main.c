6b40d70c83d51c76b72c02d75cdbb88103e62027
yangkui <752544765@qq.com>
2022-05-31 22:14:39 +0800

    add sql-parser module

---
+++ subprojects/sql-parser/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *
+ *
+ * Sql statement parser unit test
+ *
+ * @author GZYangKui
+ */
+int main(int argc, char *argv[])
+{
+    printf("Sql statement abstract syntax tree module!\n");
+    return 0;
+}
---
+++ subprojects/sql-parser/sql-ast.c
@@ -0,0 +1,8 @@
+/**
+ *
+ *
+ *  Sql statement AST implement
+ *
+ *
+ **/
+
