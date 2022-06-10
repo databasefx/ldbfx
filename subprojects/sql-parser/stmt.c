ae2481b208db3e30ba2458d3e28c169cb8dc7e7d
yangkui <752544765@qq.com>
2022-06-10 22:07:34 +0800

    fix bug

--- subprojects/sql-parser/stmt.c
+++ subprojects/sql-parser/stmt.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "stmt.h"
 #include "lex.h"
+#include "str-util.h"
 
 extern ASTNode SQLParser_expression_statement(SQLParser *parser) {
     Token token = SQLParser_token(parser);
@@ -15,17 +16,17 @@ extern ASTNode SQLParser_expression_statement(SQLParser *parser) {
             || token.kind == GRANTER_THAN_EQUAL) {
             stmt = SQLParser_ast_new(t.kind, NULL);
             Token t1 = SQLParser_token(parser);
-            stmt->kinds[0] = SQLParser_ast_new(token.kind, token.value);
-            stmt->kinds[1] = SQLParser_ast_new(VALUE, t1.value);
-        } else if(t.kind == IS){
-                Token t1 = SQLParser_token(parser);
-                if(t1.kind == _NULL ||t1.kind == NOT_NULL){
-                    stmt = SQLParser_ast_new(t1.kind,NULL);
-                    stmt->kinds[0] = SQLParser_ast_new(ID,token.value);
-                } else{
-                    printf("ExpStmt: 'null' or 'not null' was expected!");
-                }
-        } else{
+            stmt->kinds[0] = SQLParser_ast_new(ID, SQLParser_str_dump(token.value));
+            stmt->kinds[1] = SQLParser_ast_new(VALUE, SQLParser_str_dump(t1.value));
+        } else if (t.kind == IS) {
+            Token t1 = SQLParser_token(parser);
+            if (t1.kind == _NULL || t1.kind == NOT_NULL) {
+                stmt = SQLParser_ast_new(t1.kind, NULL);
+                stmt->kinds[0] = SQLParser_ast_new(ID, SQLParser_str_dump(token.value));
+            } else {
+                printf("ExpStmt: 'null' or 'not null' was expected!");
+            }
+        } else {
             printf("ExpStmt:Assign or Compare or logic symbol was required!\n");
         }
     } else {
